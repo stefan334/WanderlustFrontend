c23fe733683c1f05396de8992597863e5eb382ad
stefan334 <stefan.ivan334@gmail.com>
2023-08-07 00:28:02 +0300

    Markers now display correct images

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 
 import 'leaflet.markercluster';
@@ -11,62 +12,63 @@ import 'leaflet.markercluster';
 })
 export class HomePageComponent implements OnInit {
   username: string = "";
+  images: any[] = [];
 
-  constructor(public cookieService: CookieService) { }
+  constructor(private http: HttpClient,
+    public cookieService: CookieService) { }
 
-  ngOnInit(): void {
-    // Initialize the map
-    const map = L.map('map', { scrollWheelZoom: false }).setView([0, 0], 2);
-
-    // Add the tile layer to the map
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-      maxZoom: 20
-    }).addTo(map);
-
-    // Create a custom icon for the marker
-    const customIcon = L.icon({
-      iconUrl: 'assets/explorer_satin.png', // Replace with the URL to your custom marker icon
-      iconSize: [32, 32], // Set the size of the custom icon
-      iconAnchor: [16, 32], // Set the anchor point of the custom icon
-      popupAnchor: [0, -32], // Offset the popup
-    });
-
-    // Replace the latitude and longitude with the desired location of the marker
-    const markerLatLng = L.latLng(35.6895, 139.6917);
-
-    // Create the marker with the custom icon
-    const marker = L.marker(markerLatLng).addTo(map);
-
-    // Add a popup to the marker and make it always show
-    marker.bindPopup('<div><img id="popup-image" src="assets/explorer_satin.png" alt="Image Popup" width="100"></div>').openPopup();
+    ngOnInit(): void {
+      // Initialize the map
+      const map = L.map('map', { scrollWheelZoom: false }).setView([0, 0], 2);
     
-   
-    // Event listener to handle the popupopen event
-    marker.on('popupopen', () => {
-      // Get the image element from the popup
-      const imageElement = document.getElementById('popup-image') as HTMLImageElement;
-  
-      // Add a click event listener to open the modal on image click
-      imageElement.addEventListener('click', () => {
-        this.openModal(imageElement.src);
+      // Add the tile layer to the map
+      L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20
+      }).addTo(map);
+    
+      this.http.get('http://localhost:8080/getAllImages').subscribe((images) => {
+        console.log("Response:")
+        console.log(images)
+        this.images = images as any[];
+    
+        // Create markers for each image
+        for(const image of this.images){
+          if(image.latitude != null && image.longitude != null){
+          const markerLatLng = L.latLng(image.latitude, image.longitude);
+          const marker = L.marker(markerLatLng).addTo(map);
+          marker.bindPopup(`<div><img id="popup-image" src="${image.filePath}" alt="${image.name}" width="100"></div>`).openPopup();
+        
+          // Event listener to handle the popupopen event
+          marker.on('popupopen', () => {
+            // Get the image element from the popup
+            const imageElement = document.getElementById('popup-image') as HTMLImageElement;
+        
+            // Add a click event listener to open the modal on image click
+            imageElement.addEventListener('click', () => {
+              this.openModal(imageElement.src);
+            });
+          });
+        
+          // Event listener to handle the popupclose event
+          marker.on('popupclose', () => {
+            // Get the image element from the popup
+            const imageElement = document.getElementById('popup-image') as HTMLImageElement;
+        
+            // Remove the click event listener to prevent multiple event bindings
+            imageElement.removeEventListener('click', () => {});
+          });
+        }
+      }
       });
-    });
-  
-    // Event listener to handle the popupclose event
-    marker.on('popupclose', () => {
-      // Get the image element from the popup
-      const imageElement = document.getElementById('popup-image') as HTMLImageElement;
-  
-      // Remove the click event listener to prevent multiple event bindings
-      imageElement.removeEventListener('click', () => {});
-    });
-  
-    // Event listener to handle the map click event to close the modal
-    map.on('click', () => {
-      const modal = document.getElementById('imageModal')!;
-      modal.style.display = 'none';
-    });
-  }
+    
+      
+      // Event listener to handle the map click event to close the modal
+      map.on('click', () => {
+        const modal = document.getElementById('imageModal')!;
+        modal.style.display = 'none';
+      });
+    }
+    
 
 
    openModal(imageUrl: string) {
--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -1,34 +1,112 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { NavbarComponent } from '../navbar/navbar.component';
+/// <reference types="@types/googlemaps" />
+declare var google: any;
+
+
 @Component({
   selector: 'app-image-upload',
   templateUrl: './image-upload.component.html',
   styleUrls: ['./image-upload.component.scss']
 })
-export class ImageUploadComponent {
+export class ImageUploadComponent implements AfterViewInit{
   selectedImage!: File;
   imageName: string = '';
+  latitude: number | null = null;
+  longitude: number | null = null;
   imageLocation: string = '';
+  @ViewChild('autocompleteInput') autocompleteInput!: ElementRef;
 
   constructor(private http: HttpClient, public cookieService: CookieService) {}
 
+
+  // ... Your existing code ...
+  ngAfterViewInit(): void {
+
+    console.log('ngAfterViewInit called');
+    // Ensure that the google object and the google.maps.places.AutocompleteService class are available
+      // Set up the autocomplete on the 'location' input field
+      const autocompleteInput = this.autocompleteInput.nativeElement;
+      const autocompleteService = new google.maps.places.AutocompleteService();
+      const placesService = new google.maps.places.PlacesService(autocompleteInput);
+  
+      const options = {
+        types: ['geocode'],
+        componentRestrictions: { country: 'jp' } // Restrict results to Japan
+      };
+  
+      // Add a listener to handle input changes and update predictions
+      autocompleteInput.addEventListener('input', () => {
+        const query = autocompleteInput.value;
+        if (query.length > 0) {
+          autocompleteService.getPlacePredictions({ input: query, ...options }, (predictions: any[], status: any) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK && predictions) {
+              // Create an array of suggested location names
+              const suggestions = predictions.map((prediction) => prediction.description);
+              // Update the autocomplete options
+              autocompleteInput.setAttribute('list', 'suggestions-list');
+              const datalist = document.getElementById('suggestions-list');
+              if (datalist) {
+                datalist.innerHTML = '';
+                suggestions.forEach((suggestion) => {
+                  const option = document.createElement('option');
+                  option.value = suggestion;
+                  datalist.appendChild(option);
+                });
+              }
+            }
+          });
+        }
+      });
+    
+  }
+  
   onFileSelected(event: any) {
     this.selectedImage = event.target.files[0] as File;
   }
 
-  uploadImage() {
-     // Replace with the user's email
-     const token = this.cookieService.get('token');
-     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-     const userEmail = NavbarComponent.getUserEmailFromToken(token);
+  // Function to convert location name to latitude and longitude using Google Maps Geocoding API
+  async getLocationCoordinates(location: string): Promise<any[]> {
+    const geocoder = new google.maps.Geocoder();
+
+    return new Promise((resolve, reject) => {
+      geocoder.geocode({ address: location }, (results: any[], status: any) => {
+        if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+          resolve(results);
+        } else {
+          reject(new Error('No results found for the provided location.'));
+        }
+      });
+    });
+  }
+
+  async uploadImage() {
+    // Replace with the user's email
+    const token = this.cookieService.get('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const userEmail = NavbarComponent.getUserEmailFromToken(token);
+
+    // Convert location name to latitude and longitude
+    if (this.imageLocation) {
+      try {
+        const geocodeResponse = await this.getLocationCoordinates(this.imageLocation);
+        if (geocodeResponse.length > 0) {
+          this.latitude = geocodeResponse[0].geometry.location.lat();
+          this.longitude = geocodeResponse[0].geometry.location.lng();
+        }
+      } catch (error) {
+        console.error('Failed to geocode location', error);
+      }
+    }
+    console.log(this.latitude)
     const formData: FormData = new FormData();
     formData.append('email', userEmail!);
     formData.append('image', this.selectedImage);
     formData.append('name', this.imageName);
-    formData.append('location', this.imageLocation);
-
+    formData.append('latitude', this.latitude?.toString() || '');
+    formData.append('longitude', this.longitude?.toString() || '');
 
     headers.append('Accept', 'application/json');
 
